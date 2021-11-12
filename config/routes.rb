0b5cedf6b099496956e8424a1cb906be3361911c
christine gouws <christine.gouws@gmail.com>
2021-11-12 11:42:30 +0100

    new push for root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index"
+
   resources :lists, only: [:create, :index, :show, :new] do
     resources :bookmarks, only: [:create,:new]
   end
